f2dbd1bb2e534f110b9f53a8da31d230e8fd531d
Matyi Krista <matyas@matyaskrista.com>
2022-12-21 21:04:35 +0000

    fix: remove postprocessing

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,10 +1,4 @@
 import { OrbitControls, Environment } from '@react-three/drei';
-import {
-  Bloom,
-  DepthOfField,
-  EffectComposer,
-  Vignette,
-} from '@react-three/postprocessing';
 import { Board } from '../board/Board';
 import { generateTiles } from '../../utils/generateTiles';
 import { useGameStore } from '../../hooks/useGameStore';
@@ -19,15 +13,6 @@ const Game = () => {
       <color args={['#fff']} attach='background' />
       <Perf position='top-left' />
       <OrbitControls maxDistance={10} minDistance={1.5} enablePan={false} />
-      <EffectComposer multisampling={4}>
-        <Vignette offset={0.3} />
-        <Bloom luminanceThreshold={0} mipmapBlur intensity={0.2} />
-        <DepthOfField
-          focusDistance={0.025}
-          focalLength={0.025}
-          bokehScale={2}
-        />
-      </EffectComposer>
       <Environment preset='sunset' />
       <group rotation-x={-Math.PI * 0.5}>
         <Board />
