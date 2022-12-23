c46a316488660489448024499f48c610558087f7
Matyi Krista <matyas@matyaskrista.com>
2022-12-23 13:26:43 +0000

    fix: generation consts

--- src/consts/generation.ts
+++ src/consts/generation.ts
@@ -1,11 +1,10 @@
-export const GENRATION_DEFAULT_SETTINGS = {
-  radius: 10,
-  segments: 6,
-  seed: Math.random(),
-  height: 1,
-  scale: 0.2,
-  detail: 0.5,
-  fuzzyness: 0.25,
-  playersNum: 6,
-  nodesPerPlayer: 2,
+export const GENERATION = {
+  RADIUS: 10,
+  SEGMENTS: 6,
+  SEED: Math.random(),
+  SCALE: 0.2,
+  DETAIL: 0.5,
+  FUZZY: 0.25,
+  PLAYERS_NUM: 6,
+  NODES_PER_PLAYER: 2,
 };
--- src/utils/generateHexagonScatter.ts
+++ src/utils/generateHexagonScatter.ts
@@ -1,20 +1,19 @@
 import { Vector2, Vector3 } from 'three';
 import { CoordinateConfig } from '../types/config';
 import { v4 as uuid } from 'uuid';
-import { GENRATION_DEFAULT_SETTINGS } from '../consts/generation';
+import { GENERATION } from '../consts/generation';
 
 const GAP = 0.2;
-const { radius, playersNum, nodesPerPlayer, segments } =
-  GENRATION_DEFAULT_SETTINGS;
+const { RADIUS, PLAYERS_NUM, NODES_PER_PLAYER, SEGMENTS } = GENERATION;
 
-const getRingLength = (n: number): number => n * segments - 1;
+const getRingLength = (n: number): number => n * SEGMENTS - 1;
 
 const getRandomNodes = (): Vector2[] => {
   const nodes: Vector2[] = [];
-  const angle = (Math.PI * 2) / playersNum;
-  const nodesNum = playersNum * nodesPerPlayer;
+  const angle = (Math.PI * 2) / PLAYERS_NUM;
+  const nodesNum = PLAYERS_NUM * NODES_PER_PLAYER;
   for (let i = 0; i < nodesNum; i++) {
-    const radPlacement = Math.floor(Math.random() * radius * 0.7 + 2);
+    const radPlacement = Math.floor(Math.random() * RADIUS * 0.7 + 2);
     const sdPlacement = Math.floor(
       Math.abs(Math.sin(angle * i) * radPlacement * Math.random()) * nodesNum
     );
@@ -24,7 +23,7 @@ const getRandomNodes = (): Vector2[] => {
 };
 
 export const generateHexagonScatter = () => {
-  const { radius } = GENRATION_DEFAULT_SETTINGS;
+  const { RADIUS } = GENERATION;
   const points: CoordinateConfig[] = [];
   // Center point
   points.push({
@@ -34,7 +33,7 @@ export const generateHexagonScatter = () => {
   });
   // Transformation tools
   const unit = GAP;
-  const angle = (Math.PI / segments) * 2;
+  const angle = (Math.PI / SEGMENTS) * 2;
   const axis = new Vector3(0, 0, 1);
   const axisVector = new Vector3(0, -unit, 0);
   const sideVector = new Vector3(0, unit, 0).applyAxisAngle(axis, -angle);
@@ -43,15 +42,13 @@ export const generateHexagonScatter = () => {
   // Generate resource node positions
   const nodes = getRandomNodes();
 
-  for (let seg = 0; seg < segments; seg++) {
-    for (let ax = 1; ax <= radius; ax++) {
+  for (let seg = 0; seg < SEGMENTS; seg++) {
+    for (let ax = 1; ax <= RADIUS; ax++) {
       for (let sd = 0; sd < ax; sd++) {
         const ring = ax;
         const ringPosition = sd + ax * seg;
         const ringWidth = getRingLength(ring);
-        const ringUnit = Math.floor(
-          ringWidth / GENRATION_DEFAULT_SETTINGS.playersNum + 1
-        );
+        const ringUnit = Math.floor(ringWidth / PLAYERS_NUM + 1);
 
         tempV3
           .copy(axisVector)
@@ -66,7 +63,7 @@ export const generateHexagonScatter = () => {
         };
 
         if (
-          ring === Math.floor(radius * 0.99) &&
+          ring === Math.floor(RADIUS * 0.99) &&
           ringPosition % ringUnit === 0
         ) {
           console.log(ringWidth, ringPosition);
