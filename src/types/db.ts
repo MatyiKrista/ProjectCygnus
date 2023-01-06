290ba66f18c6db966aab02fe019e6701a7d02153
Matyi Krista <matyas@matyaskrista.com>
2023-01-06 11:32:34 +0000

    feat: game types

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,3 +1,5 @@
+import { UUID } from './game';
+
 export type User = {
   createdAt: Date;
   email: string;
@@ -5,4 +7,39 @@ export type User = {
   photoUrl: string | null;
   updatedAt: Date;
   friends: string[];
+};
+
+export type ActionType = 'move' | 'attack' | 'defend';
+
+export interface BaseAction<T extends ActionType> {
+  type: T;
+  unit: UUID;
+}
+
+export interface MoveAction extends BaseAction<'move'> {
+  from: UUID;
+  to: UUID;
+}
+
+export interface AttackAction extends BaseAction<'attack'> {
+  target: UUID;
+}
+
+export interface DefendAction extends BaseAction<'defend'> {}
+
+export type Action = MoveAction | AttackAction | DefendAction;
+
+export type Turn = {
+  number: number;
+  player: string;
+  actions: Action[];
+};
+
+export type Game = {
+  createdAt: Date;
+  createdBy: string;
+  players: string[];
+  updatedAt: Date;
+  turns: Turn[];
+  currentTurn: number;
 };
